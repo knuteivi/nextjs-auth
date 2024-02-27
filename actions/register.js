ed9b2e089b226a4bacb7baeea2f5dd5df5246384
knuteivi <knuteivi@gmail.com>
2024-02-27 12:09:18 +0100

    Add User table and bcrypt

--- actions/register.js
+++ actions/register.js
@@ -1,12 +1,35 @@
 "use server";
 
 import { RegisterSchema } from "@/schemas";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
-export const register = (values) => {
+export const register = async (values) => {
     const validatedFields = RegisterSchema.safeParse(values);
 
     if(!validatedFields){
         return { error: "Invalid fields!"};
     }
+
+    const {email, password, name} = validatedFields.data;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const existingUser = await db.user.findUnique({
+        where: {
+            email,
+        },
+    });
+
+    if(existingUser){
+        return {error: "Email already in use!"};
+    }
+
+    await db.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+        }
+    });
+
     return {success: "Successfully registered!"};
 };
